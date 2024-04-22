d621032148f7d3394822b801f5624daf2a3a711f
carlschader <carlschader@gmail.com>
2024-04-22 02:11:35 -0600

    logging and working scheduler (no warmup though :(

--- src/module.py
+++ src/module.py
@@ -2,7 +2,7 @@
 # the embeddings against the class centers across all classes,
 # but instead just against the class centers within the batch.
 
-import torch
+import torch, multiprocessing
 import torch.nn as nn
 import torch.nn.functional as F
 import lightning as L
@@ -20,7 +20,7 @@ def arcface_loss(embeddings, targets, centers, m=0.5, s=64.0):
     margin_distances = s*torch.cos(angles)
     return cross_entropy(margin_distances, targets)
 
-class LabeledContrastiveModule(L.LightningModule):
+class LabeledContrastiveEncoder(L.LightningModule):
     def __init__(
             self, 
             backbone, 
@@ -49,6 +49,8 @@ class LabeledContrastiveModule(L.LightningModule):
 
         # initialize class centers on a sphere
         self.centers = torch.tensor(sphere_lattice(embedding_dim, num_classes), dtype=torch.float32)
+        
+        self.save_hyperparameters(ignore=['backbone'])
 
     def on_fit_start(self):
         self.centers = self.centers.to(self.device)
@@ -57,30 +59,39 @@ class LabeledContrastiveModule(L.LightningModule):
         x, y = batch
         z = self.encoder(x)
         norms = F.normalize(z, p=2, dim=1)
-        return self.loss_fn(norms, y, self.centers, self.margin, self.scale)
+        loss = self.loss_fn(norms, y, self.centers, self.margin, self.scale)
+        self.log('train_loss', loss, prog_bar=True)
+        return loss
 
     def validation_step(self, batch, batch_idx):
         x, y = batch
         z = self.encoder(x)
         norms = F.normalize(z, p=2, dim=1)
-        return self.loss_fn(norms, y, self.centers, self.margin, self.scale)
+        loss = self.loss_fn(norms, y, self.centers, self.margin, self.scale)
+        self.log('val_loss', loss, prog_bar=True)
+        return loss
 
     def configure_optimizers(self):
         optimizer = optim.AdamW(self.parameters(), lr=self.learning_rate, weight_decay=self.weight_decay)
-        scheduler = optim.lr_scheduler.SequentialLR(optimizer, schedulers=[
-            optim.lr_scheduler.LinearLR(optimizer, 0.33, 1.0, total_iters=5),
-            optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=3, factor=0.5),
-        ],
-        milestones=[5])
+        # scheduler = optim.lr_scheduler.SequentialLR(optimizer, schedulers=[
+        #     optim.lr_scheduler.LinearLR(optimizer, 0.33, 1.0, total_iters=5),
+        #     optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=3, factor=0.5),
+        # ],
+        # milestones=[1])
+        scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=3, factor=0.5)
         return {
             'optimizer': optimizer,
-            'lr_scheduler': scheduler,
+            'lr_scheduler': {
+                'scheduler': scheduler,
+                "monitor": "val_loss",
+            }
         }
 
 if __name__ == '__main__':
     import time, argparse
     from torchvision import datasets
     from transform import make_transform
+    from lightning.pytorch.callbacks import ModelCheckpoint
 
     # from transformers import Dinov2Model
 
@@ -97,6 +108,10 @@ if __name__ == '__main__':
     num_classes = len(dataset.classes)
     embedding_dim = 128
     backbone_out_dim = 384 
+    batch_size = 128
+    epochs = 100
+    dataloader_workers = max((multiprocessing.cpu_count() // 2) - 1, 0)
+    print('num_workers:', dataloader_workers)
 
     train_set_size = int(len(dataset) * 0.98)
     valid_set_size = len(dataset) - train_set_size
@@ -104,20 +119,27 @@ if __name__ == '__main__':
     seed = torch.Generator().manual_seed(42)
     train_set, valid_set = torch.utils.data.random_split(dataset, [train_set_size, valid_set_size], generator=seed)
 
-    train_loader = torch.utils.data.DataLoader(train_set)
-    valid_loader = torch.utils.data.DataLoader(valid_set)
-    
-    
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=dataloader_workers)
+    valid_loader = torch.utils.data.DataLoader(valid_set, batch_size=batch_size, shuffle=False, num_workers=dataloader_workers)
 
     print('loading backbone')
     # backbone = Dinov2Model.from_pretrained("facebook/dinov2-base").base_model
     backbone = torch.hub.load('facebookresearch/dinov2', 'dinov2_vits14_reg')
 
     print('initializing lightining module')
-    lightning_module = LabeledContrastiveModule(backbone, backbone_out_dim, num_classes, embedding_dim=embedding_dim)
+    lightning_module = LabeledContrastiveEncoder(backbone, backbone_out_dim, num_classes, embedding_dim=embedding_dim)
+
+    checkpoint_callback = ModelCheckpoint(
+        monitor='val_loss',
+        mode='min',
+        save_last=True,
+        every_n_epochs=1,
+        save_top_k=1,
+        filename='arcface-{epoch:02d}-{val_loss:.2f}',
+    )
 
     print('initializing trainer')
-    trainer = L.Trainer(max_epochs=100)
+    trainer = L.Trainer(max_epochs=epochs, callbacks=[checkpoint_callback])
 
     print('training')
     trainer.fit(lightning_module, train_loader, valid_loader)
